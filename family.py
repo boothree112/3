04498f24360efca36ca87e69c3a37acd7415ac52
boothree112 <137603571+boothree112@users.noreply.github.com>
2023-11-20 11:11:13 +0100

    protect

---
+++ family.py
@@ -0,0 +1,37 @@
+[![Build Status](https://travis-ci.org/donnemartin/haxor-news.svg?branch=master)](https://travis-ci.org/donnemartin/haxor-news) [![Dependency Status](https://gemnasium.com/donnemartin/haxor-news.svg)](https://gemnasium.com/donnemartin/haxor-news)
+[![PyPI version](https://badge.fury.io/py/haxor-news.svg)](http://badge.fury.io/py/haxor-news) [![PyPI](https://img.shields.io/pypi/pyversions/haxor-news.svg)](https://pypi.python.org/pypi/haxor-news/) [![License](http://img.shields.io/:license-apache-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0.html)
+
+==========
+To view the latest `README`, `docs`, and `code` visit the GitHub repo:
+
+To submit bugs or feature requests, visit the issue tracker:
+
+Changelog
+
+------------------
+
+### Bug Fixes
+
+* [#94](https://github.com/donnemartin/haxor-news/pull/94) - Update to `prompt-toolkit` 1.0.0, which includes a number of performance improvements (especially noticeable on Windows) and bug fixes.
+
+### Bug Fixes
+
+
+0.4.0 (2016-05-30)
+
+
+* [#52](https://github.com/donnemartin/haxor-news/issues/52) - Add `exit` and `quit` commands, which can be used instead of `ctrl-d`.
+
+* [#36](https://github.com/donnemartin/haxor-news/issues/36) - Fix crash caused by Unicode comments on Windows.
+* Fix some comments and docstrings.
+### Updates
+
+* [#48](https://github.com/donnemartin/haxor-news/issues/48), [#50](https://github.com/donnemartin/haxor-news/issues/50) - Update latest monthly hiring post ids.
+* Update `README`:
+    * Update intro
+    * Update comments discussion and examples
+    * Add note about OS X 10.11 pip installation issue
+
+------------------
+
+- Initial release.
